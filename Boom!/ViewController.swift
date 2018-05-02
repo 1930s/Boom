844016c000ce24ff69e6c7350b014b6d41cdc27a
JiYoung <jyc367@nyu.edu>
2018-05-02 12:01:10 -0400

    Initial Commit

---
+++ Boom!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Boom!
+//
+//  Created by 지영 전 on 5/2/18.
+//  Copyright © 2018 지영 전. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
